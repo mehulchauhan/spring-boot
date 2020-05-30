bd9d0e63a113e0dd2e1c8cabf5bd852da2e4abf5
Mehul Chauhan <mehul.chauhan@digitas.com>
2020-05-30 19:19:37 +0530

    initial mavan project

---
+++ spring-boot-rbac/src/test/java/com/dtech/restservices/SpringBootRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtech.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
